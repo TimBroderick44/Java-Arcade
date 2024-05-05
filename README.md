13b0059b627255e78503c78cadebed09828bd2bc
Tim Broderick <timbroderick44@gmail.com>
2024-05-05 21:49:16 +0900

    Ready for Review

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# Java-Arcade
-A collection of games made in Java
+
+<h1> Welcome to: 
+
+![Such simple.....](./assets/Name.PNG)
+***<p style="font-size: 26"> The following games are available to play: </p>***
+
+![Such simple.....](./assets/Start.PNG)
+![Such simple.....](./assets/Minesweeper.PNG)
+![Such simple.....](./assets/TicTacToe.PNG)
+![Such simple.....](./assets/Hangman.PNG)
+![Such simple.....](./assets/TrueOrFalse.PNG)
+## What did I use to create it?
+
+This project uses:
+
+-  Java
+
+## Features of the Project:
+
+-  Users are able to play a multitude of games and have their scores recorded.
+-  Scores are carried over between games. 
+-  Users are able to navigate seemlessly between games. 
+
+<h1 style="font-weight: 900"> Here are some of the lessons I learnt:</h1>
+
+### Using Java:
+
+### The Convenience of Abstract classes and Interfaces:
+
+-  From the beginning, I wanted to focus on using abstract classes and interfaces so that I would be able to become more familiar with them. As a result, I'm now a lot more comfortable using java and understand the importance of these tools
+
+### Objects, classes and methods:
+
+- Transitioning from a JavaScript-focused development environment, primarily using React, to Java required a different approach. It was in no way smooth but overall it was a very educational experience. Java has a strong emphasis on class-based object-oriented programming which had allowed me to delve deeper into the concepts of objects, classes, and methods. 
+
+## What I want to include in the future:
+
+## Have more functionality:
+
+-   [ ] Include more games
+-   [ ] Allow for a GUI (give the user a choice of CLI or GUI)
+-   [ ] Introduce Multiplayer options
+
+## Thank You!
+
+Thank you for taking the time to look at this project. I really hope you enjoy it.
+Feel free to reach out and ask any questions.
+
+[Tim Broderick]
--- src/common/OutputHandler.java
+++ src/common/OutputHandler.java
@@ -22,36 +22,40 @@ public class OutputHandler {
         int columns = game.getColumns();
         game.displayTitle();
     
-        // Print column headers
-        System.out.print("   "); // Spacing for row labels
+        // Print column labels on top (1, 2, 3, ...)
+        System.out.print("   "); 
         for (int j = 0; j < columns; j++) {
             System.out.printf("%3d ", j + 1);
         }
         System.out.println();
     
-        // Print top border of the grid
+        // Print top border
         System.out.print("   +");
         for (int j = 0; j < columns; j++) {
             System.out.print("---+");
         }
         System.out.println();
     
+        // Print row labels on the left (A, B, C, ...)
         for (int i = 0; i < rows; i++) {
-            // Print row label
             System.out.printf("%2c |", 'A' + i);
             for (int j = 0; j < columns; j++) {
                 String cell = game.getCell(i, j);
-                boolean isFlagged = game.isFlagged(i, j); // Assuming the Minesweeper class has an isFlagged method
+                // Check if cell is flagged
+                boolean isFlagged = game.isFlagged(i, j);
                 if (isFlagged) {
-                    System.out.print(" F |"); // Display 'F' for flagged cells
+                    // Print F for flagged cell
+                    System.out.print(" F |"); 
                 } else {
-                    System.out.print(" " + cell + " |"); // Display cell content
+                    // Print cell content
+                    System.out.print(" " + cell + " |"); 
                 }
             }
             System.out.println();
     
-            // Print row divider
+            // Print row separator
             System.out.print("   +");
+            // Print separator between cells
             for (int j = 0; j < columns; j++) {
                 System.out.print("---+");
             }
@@ -64,21 +68,24 @@ public class OutputHandler {
         int columns = ticTacToe.getColumns();
     
         // Print row labels on top
-        System.out.print("  "); // Leading spaces for alignment
+        System.out.print("  "); 
         for (int i = 0; i < rows; i++) {
-            System.out.print(" " + (i + 1) + "  "); // Print row numbers 1, 2, 3 across the X axis
+            System.out.print(" " + (i + 1) + "  "); 
         }
         System.out.println();
-        System.out.println("   ------------"); // Adjust the line length as needed
+        System.out.println("   ------------"); 
     
+        // Print board
         for (int j = 0; j < columns; j++) {
-            System.out.print((char) ('A' + j) + " |"); // Print column labels A, B, C down the Y axis
+            // Print column label on the left (A, B, C, ...)
+            System.out.print((char) ('A' + j) + " |"); 
             for (int i = 0; i < rows; i++) {
-                char cell = ticTacToe.getCell(i, j).charAt(0); // Use getCell(row, column)
+                // Print cell content (X, O, or empty)
+                char cell = ticTacToe.getCell(i, j).charAt(0); 
                 System.out.print((cell == '\u0000' ? "   |" : " " + cell + " |"));
             }
             System.out.println();
-            System.out.println("   ------------"); // Adjust the line length as needed
+            System.out.println("   ------------");
         }
     }
     
